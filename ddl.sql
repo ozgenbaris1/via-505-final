36896f6cac6c1f3a30154f4364fd8c5e73ac7a7d
Barış Özgen <ozgenb15@itu.edu.tr>
2022-12-14 15:23:39 +0300

    Add foreign key references while creating tables

--- ddl.sql
+++ ddl.sql
@@ -1,114 +1,103 @@
-CREATE DATABASE gotur;
+CREATE DATABASE IF NOT EXISTS gotur;
 
 USE gotur;
 
-CREATE TABLE country (
+CREATE TABLE IF NOT EXISTS country (
 	country_id           int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	name                 varchar(100)  NOT NULL
  );
 
-CREATE TABLE city (
+CREATE TABLE IF NOT EXISTS city (
 	city_id              int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	name                 varchar(100)      ,
-	country_id           int  NOT NULL
+	country_id           int  NOT NULL    ,
+	CONSTRAINT fk_city_country FOREIGN KEY ( country_id ) REFERENCES country( country_id )
  );
 
-ALTER TABLE city ADD CONSTRAINT fk_city_country FOREIGN KEY ( country_id ) REFERENCES country( country_id );
-
-CREATE TABLE district (
+CREATE TABLE IF NOT EXISTS district (
 	district_id          int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	name                 varchar(100)  NOT NULL    ,
-	city_id              int  NOT NULL
+	city_id              int  NOT NULL    ,
+	CONSTRAINT fk_district_city FOREIGN KEY ( city_id ) REFERENCES city( city_id )
  );
 
-ALTER TABLE district ADD CONSTRAINT fk_district_city FOREIGN KEY ( city_id ) REFERENCES city( city_id );
-
-CREATE TABLE address (
+CREATE TABLE IF NOT EXISTS address (
 	address_id           int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	latitude             float  NOT NULL    ,
 	longitude            float  NOT NULL    ,
 	detail               varchar(256)  NOT NULL    ,
 	country_id           int  NOT NULL    ,
 	city_id              int  NOT NULL    ,
-	district_id          int  NOT NULL
+	district_id          int  NOT NULL    ,
+	CONSTRAINT fk_address_country FOREIGN KEY ( country_id ) REFERENCES country( country_id ),
+	CONSTRAINT fk_address_city FOREIGN KEY ( city_id ) REFERENCES city( city_id ),
+	CONSTRAINT fk_address_district FOREIGN KEY ( district_id ) REFERENCES district( district_id )
  );
 
-ALTER TABLE address ADD CONSTRAINT fk_address_country FOREIGN KEY ( country_id ) REFERENCES country( country_id );
-
-ALTER TABLE address ADD CONSTRAINT fk_address_city FOREIGN KEY ( city_id ) REFERENCES city( city_id );
-
-ALTER TABLE address ADD CONSTRAINT fk_address_district FOREIGN KEY ( district_id ) REFERENCES district( district_id );
-
-CREATE TABLE customer (
+CREATE TABLE IF NOT EXISTS customer (
 	customer_id          int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	first_name           varchar(100)  NOT NULL    ,
 	last_name            varchar(100)  NOT NULL    ,
 	create_date          datetime  NOT NULL DEFAULT CURRENT_TIMESTAMP   ,
-	address_id           int  NOT NULL    
+	address_id           int  NOT NULL    ,
+	CONSTRAINT fk_customer_address FOREIGN KEY ( address_id ) REFERENCES address( address_id )
  );
 
-ALTER TABLE customer ADD CONSTRAINT fk_customer_address FOREIGN KEY ( address_id ) REFERENCES address( address_id );
-
-CREATE TABLE category (
+CREATE TABLE IF NOT EXISTS category (
 	category_id          int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	name                 varchar(100)  NOT NULL
  );
 
-CREATE TABLE product (
+CREATE TABLE IF NOT EXISTS product (
 	product_id           int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	name                 varchar(100)  NOT NULL    ,
 	price                float  NOT NULL    ,
-	category_id          int  NOT NULL
+	category_id          int  NOT NULL,
+	CONSTRAINT fk_product_category FOREIGN KEY ( category_id ) REFERENCES category( category_id )
  );
 
-ALTER TABLE product ADD CONSTRAINT fk_product_category FOREIGN KEY ( category_id ) REFERENCES category( category_id );
-
-CREATE TABLE status (
+CREATE TABLE IF NOT EXISTS status (
 	status_id            int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	definition           varchar(100)  NOT NULL
  );
 
-CREATE TABLE store (
+CREATE TABLE IF NOT EXISTS store (
 	store_id             int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	name                 varchar(100)  NOT NULL    ,
 	create_date          datetime  NOT NULL DEFAULT current_timestamp   ,
-	address_id           int  NOT NULL
+	address_id           int  NOT NULL,
+	CONSTRAINT fk_store_address FOREIGN KEY ( address_id ) REFERENCES address( address_id )
+
  );
 
 
-ALTER TABLE store ADD CONSTRAINT fk_store_address FOREIGN KEY ( address_id ) REFERENCES address( address_id );
-
-CREATE TABLE inventory (
+CREATE TABLE IF NOT EXISTS inventory (
 	store_id             int  NOT NULL    ,
 	quantity             int  NOT NULL    ,
 	product_id           int  NOT NULL    ,
-	CONSTRAINT idx_stock PRIMARY KEY ( store_id, product_id )
+	CONSTRAINT idx_stock PRIMARY KEY ( store_id, product_id ),
+	CONSTRAINT fk_inventory_store FOREIGN KEY ( store_id ) REFERENCES store( store_id ),
+	CONSTRAINT fk_inventory_product FOREIGN KEY ( product_id ) REFERENCES product( product_id )
+
  );
 
-ALTER TABLE inventory ADD CONSTRAINT fk_inventory_store FOREIGN KEY ( store_id ) REFERENCES store( store_id );
-
-ALTER TABLE inventory ADD CONSTRAINT fk_inventory_product FOREIGN KEY ( product_id ) REFERENCES product( product_id );
-
-CREATE TABLE stock_entry (
+CREATE TABLE IF NOT EXISTS stock_entry (
 	stock_entry_id       int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	create_date          datetime  NOT NULL DEFAULT CURRENT_TIMESTAMP   ,
-	store_id             int  NOT NULL
+	store_id             int  NOT NULL,
+	CONSTRAINT fk_stock_entry_store FOREIGN KEY ( store_id ) REFERENCES store( store_id )
  ) ;
 
-ALTER TABLE stock_entry ADD CONSTRAINT fk_stock_entry_store FOREIGN KEY ( store_id ) REFERENCES store( store_id );
-
-CREATE TABLE stock_entry_detail (
+CREATE TABLE IF NOT EXISTS stock_entry_detail (
 	stock_entry_id       int  NOT NULL    ,
 	quantity             int  NOT NULL    ,
 	product_id           int  NOT NULL    ,
-	CONSTRAINT idx_stock_entry_detail PRIMARY KEY ( stock_entry_id, product_id )
+	CONSTRAINT idx_stock_entry_detail PRIMARY KEY ( stock_entry_id, product_id ),
+	CONSTRAINT fk_stock_entry_detail FOREIGN KEY ( stock_entry_id ) REFERENCES stock_entry( stock_entry_id ),
+	CONSTRAINT fk_stock_entry_detail_product FOREIGN KEY ( product_id ) REFERENCES product( product_id )
  );
 
-ALTER TABLE stock_entry_detail ADD CONSTRAINT fk_stock_entry_detail FOREIGN KEY ( stock_entry_id ) REFERENCES stock_entry( stock_entry_id );
-
-ALTER TABLE stock_entry_detail ADD CONSTRAINT fk_stock_entry_detail_product FOREIGN KEY ( product_id ) REFERENCES product( product_id );
-
-CREATE TABLE courier (
+CREATE TABLE IF NOT EXISTS courier (
 	courier_id           int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	first_name           varchar(100)  NOT NULL    ,
 	last_name            varchar(100)  NOT NULL    ,
@@ -116,47 +105,39 @@ CREATE TABLE courier (
 	longitude            float  NOT NULL
  );
 
-CREATE TABLE customer_order (
+CREATE TABLE IF NOT EXISTS customer_order (
 	customer_order_id    int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	create_date          datetime  NOT NULL DEFAULT CURRENT_TIMESTAMP   ,
 	customer_id          int  NOT NULL    ,
 	status_id            int  NOT NULL    ,
 	courier_id           int  NOT NULL    ,
-	store_id             int  NOT NULL
+	store_id             int  NOT NULL,
+	CONSTRAINT fk_customer_order_customer FOREIGN KEY ( customer_id ) REFERENCES customer( customer_id ),
+	CONSTRAINT fk_customer_order_status FOREIGN KEY ( status_id ) REFERENCES status( status_id ),
+	CONSTRAINT fk_customer_order_courier FOREIGN KEY ( courier_id ) REFERENCES courier( courier_id ),
+	CONSTRAINT fk_customer_order_store FOREIGN KEY ( store_id ) REFERENCES store( store_id )
  );
 
-ALTER TABLE customer_order ADD CONSTRAINT fk_customer_order_customer FOREIGN KEY ( customer_id ) REFERENCES customer( customer_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-ALTER TABLE customer_order ADD CONSTRAINT fk_customer_order_status FOREIGN KEY ( status_id ) REFERENCES status( status_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-ALTER TABLE customer_order ADD CONSTRAINT fk_customer_order_courier FOREIGN KEY ( courier_id ) REFERENCES courier( courier_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-ALTER TABLE customer_order ADD CONSTRAINT fk_customer_order_store FOREIGN KEY ( store_id ) REFERENCES store( store_id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 
-
-CREATE TABLE customer_order_detail (
+CREATE TABLE IF NOT EXISTS customer_order_detail (
 	customer_order_id    int  NOT NULL    ,
 	quantity             int  NOT NULL    ,
 	product_id           int  NOT NULL    ,
-	CONSTRAINT idx_customer_order_detail PRIMARY KEY ( customer_order_id, product_id )
+	CONSTRAINT idx_customer_order_detail PRIMARY KEY ( customer_order_id, product_id ),
+	CONSTRAINT fk_order FOREIGN KEY ( customer_order_id ) REFERENCES customer_order( customer_order_id ),
+	CONSTRAINT fk_product FOREIGN KEY ( product_id ) REFERENCES product( product_id )
+
  );
 
-ALTER TABLE customer_order_detail ADD CONSTRAINT fk_order FOREIGN KEY ( customer_order_id ) REFERENCES customer_order( customer_order_id );
-
-ALTER TABLE customer_order_detail ADD CONSTRAINT fk_product FOREIGN KEY ( product_id ) REFERENCES product( product_id );
-
-CREATE TABLE rating (
+CREATE TABLE IF NOT EXISTS rating (
 	rating_id            int  NOT NULL  AUTO_INCREMENT  PRIMARY KEY,
 	point                int  NOT NULL    ,
 	create_date          datetime  NOT NULL DEFAULT CURRENT_TIMESTAMP   ,
 	customer_id          int  NOT NULL    ,
 	customer_order_id    int  NOT NULL    ,
-	courier_id           int  NOT NULL
+	courier_id           int  NOT NULL,
+	CONSTRAINT fk_rating_customer FOREIGN KEY ( customer_id ) REFERENCES customer( customer_id ),
+	CONSTRAINT fk_rating_customer_order FOREIGN KEY ( customer_order_id ) REFERENCES customer_order( customer_order_id ),
+	CONSTRAINT fk_rating_courier FOREIGN KEY ( courier_id ) REFERENCES courier( courier_id )
  );
-
-ALTER TABLE rating ADD CONSTRAINT fk_rating_customer FOREIGN KEY ( customer_id ) REFERENCES customer( customer_id );
-
-ALTER TABLE rating ADD CONSTRAINT fk_rating_customer_order FOREIGN KEY ( customer_order_id ) REFERENCES customer_order( customer_order_id );
-
-ALTER TABLE rating ADD CONSTRAINT fk_rating_courier FOREIGN KEY ( courier_id ) REFERENCES courier( courier_id );
